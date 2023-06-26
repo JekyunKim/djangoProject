59d0b07f503650f18ab9baaed476a84868808a9c
Jekyun Kim <kkyuni97@naver.com>
2023-06-26 11:14:55 +0900

    어드민 모델, bookmark 마이그레이션 추가

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from bookmark.models import Bookmark
+
 # Register your models here.
+
+# 데코레이터 패턴을 사용하지 않는 방법
+# admin.site.register(Bookmark, BookmarkAdmin)
+
+
+# 위에 코드와 아래 @, class랑 동일
+@admin.register(Bookmark)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url')
+
